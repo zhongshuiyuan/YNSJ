9e9cf5abc0efb2e3d6b8afddae456b65f267c229
971946256 <971946256@qq.com>
2017-09-04 17:03:06 +0800

    修改图层标注

--- ynsjy/src/main/java/com/titan/ynsjy/presenter/LayerLablePresenter.java
+++ ynsjy/src/main/java/com/titan/ynsjy/presenter/LayerLablePresenter.java
@@ -2,6 +2,7 @@ package com.titan.ynsjy.presenter;
 
 import android.content.Context;
 import android.graphics.Color;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.ListView;
@@ -9,7 +10,6 @@ import android.widget.ListView;
 import com.esri.core.geodatabase.GeodatabaseFeature;
 import com.esri.core.geometry.Envelope;
 import com.esri.core.map.CallbackListener;
-import com.esri.core.map.FeatureResult;
 import com.esri.core.map.Field;
 import com.esri.core.map.Graphic;
 import com.esri.core.symbol.TextSymbol;
@@ -35,15 +35,18 @@ public class LayerLablePresenter {
     public ILayerView iLayerView;
     public MyLayer myLayer;
     private long[] arrays;
-    private HashMap<Field,Boolean> checkboxMap = new HashMap<>();
+    private HashMap<Field, Boolean> checkboxMap = new HashMap<>();
+    private String[] filterField = new String[]{"OBJECTID_1", "OBJECTID_12", "OBJECTID", "Shape_Leng", "Shape_Le_1"};
 
-    public LayerLablePresenter(Context context, ILayerView layerView){
+    public LayerLablePresenter(Context context, ILayerView layerView) {
         this.mContext = context;
         this.iLayerView = layerView;
     }
 
-    /**显示选择图层的对应字段*/
-    public void showLayerAials(final View lableView, MyLayer myLayer){
+    /**
+     * 显示选择图层的对应字段
+     */
+    public void showLayerAials(final View lableView, MyLayer myLayer) {
         this.myLayer = myLayer;
         ImageView imageView = (ImageView) lableView.findViewById(R.id.attr_field_exit);
         imageView.setOnClickListener(new View.OnClickListener() {
@@ -52,69 +55,84 @@ public class LayerLablePresenter {
                 lableView.setVisibility(View.GONE);
             }
         });
-        ListView listView =(ListView) lableView.findViewById(R.id.field_list);
+        ListView listView = (ListView) lableView.findViewById(R.id.field_list);
         List<Field> fields = myLayer.getLayer().getFeatureTable().getFields();
-        for(Field f:fields){
-            if (f.getAlias().equals("OBJECTID_1") || f.getAlias().equals("OBJECTID") || f.getAlias().equals("Shape_Leng")){
+        boolean flag = false;
+        for (Field f : fields) {
+            flag = false;
+            for (String aFilterField : filterField) {
+                if (f.getName().equals(aFilterField)) {
+                    flag = true;
+                    break;
+                }
+            }
+            if (flag) {
                 continue;
             }
-            checkboxMap.put(f,false);
+            checkboxMap.put(f, false);
         }
-        LayerLableAdapter lableAdapter = new LayerLableAdapter(mContext,this,checkboxMap);
+        LayerLableAdapter lableAdapter = new LayerLableAdapter(mContext, this, checkboxMap);
         listView.setAdapter(lableAdapter);
-        BaseUtil.setHeight(lableAdapter,listView);
+        BaseUtil.setHeight(lableAdapter, listView);
 
     }
 
-    /**查询当前区域内的对应图层的小班数据*/
-    public void queryFeatures(final MyLayer myLayer,final boolean isChecked,final List<Field> fields,final int position){
+    /**
+     * 查询当前区域内的对应图层的小班数据
+     */
+    public void queryFeatures(final MyLayer myLayer, final boolean isChecked, final List<Field> fields, final int position) {
         QueryParameters q = new QueryParameters();
         q.setWhere("1=1");
         q.setInSpatialReference(iLayerView.getBaseTitleLayer().getSpatialReference());
         q.setReturnGeometry(true);
         q.setGeometry(iLayerView.getCurrentEnvelope());
         //myLayer.getLayer().setSelectionColor(0);
-        myLayer.getTable().queryFeatures(q, new CallbackListener<FeatureResult>() {
+        myLayer.getTable().queryIds(q, new CallbackListener<long[]>() {
             @Override
-            public void onCallback(FeatureResult featureResult) {
-                if (featureResult.featureCount() > 0){
-                    arrays = myLayer.getLayer().getSelectionIDs();
-                    addLableToLayer(isChecked,fields,position);
+            public void onCallback(long[] longs) {
+                if (longs.length > 0) {
+                    arrays = longs;
+                    addLableToLayer(isChecked, fields, position);
                 }
             }
 
             @Override
             public void onError(Throwable throwable) {
+
             }
         });
     }
 
-    /**添加lable*/
-    public void addLableToLayer(boolean isChecked, List<Field> fields, int position){
-        if(isChecked){
+    /**
+     * 添加lable
+     */
+    public void addLableToLayer(boolean isChecked, List<Field> fields, int position) {
+        if (isChecked) {
             iLayerView.getGraphicLayer().removeAll();
             return;
         }
-        if(arrays == null){
+        if (arrays == null) {
             return;
         }
         iLayerView.getGraphicLayer().removeAll();
-        for(long id : arrays){
+        for (long id : arrays) {
             GeodatabaseFeature feature = (GeodatabaseFeature) myLayer.getLayer().getFeature(id);
             Object obj = feature.getAttributeValue(fields.get(position));
             String text;
-            if(obj != null){
+            Log.e("tag","obj"+obj);
+            if (obj != null&&!obj.equals("")) {
                 text = obj.toString();
-            }else {
+            } else {
                 text = "空";
             }
-            TextSymbol textSymbol = new TextSymbol(20,text, Color.BLUE);
+            TextSymbol textSymbol = new TextSymbol(20, text, Color.BLUE);
             //解决中文乱码
             textSymbol.setFontFamily("DroidSansFallback.ttf");
             Envelope env = new Envelope();
             feature.getGeometry().queryEnvelope(env);
-            Graphic graphic = new Graphic(env.getCenter(),textSymbol);
+            Graphic graphic = new Graphic(env.getCenter(), textSymbol);
             iLayerView.getGraphicLayer().addGraphic(graphic);
         }
     }
+
 }
